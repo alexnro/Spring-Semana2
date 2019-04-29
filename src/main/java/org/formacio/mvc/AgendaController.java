50a4bb2e7a134e1d1966687d1e861abc65ae23bb
alexnro <alexjandron99@gmail.com>
2019-04-29 22:28:07 +0200

    Tercer caso test pasado

--- src/main/java/org/formacio/mvc/AgendaController.java
+++ src/main/java/org/formacio/mvc/AgendaController.java
@@ -1,11 +1,15 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.websocket.server.PathParam;
 
 @Controller
 public class AgendaController {
@@ -23,5 +27,11 @@ public class AgendaController {
     @ResponseBody
     public String getTelefon(String id) {
         return agenda.recupera(id).getTelefon();
+    }
+
+    @RequestMapping(path="/contacte/{id}", produces= MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Persona getPersona(@PathVariable String id) {
+        return agenda.recupera(id);
     }
 }
